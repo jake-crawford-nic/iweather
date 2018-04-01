c4d1ab0f567b3d359e46d3df19a214db43c6f873
Jake <Jake.Crawford@egov.com>
2018-03-31 20:47:28 -0400

    Import data

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Response } from '@angular/http';
 
 /*
   Generated class for the WeatherProvider provider.
@@ -9,9 +11,16 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class WeatherProvider {
+  apiKey = '3fd386cbd46ef95f';
+  url = null;
 
   constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q';
+  }
+
+  getWeather(city, state) {
+    return this.http.get(this.url + '/' + state + '/' + city + '.json');
   }
 
 }
